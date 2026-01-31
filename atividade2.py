46000990918465d3a2834e8020ecfd240f6735bd
Rafa <75946637+rafabruno263@users.noreply.github.com>
2026-01-31 10:58:42 -0300

    Add files via upload

---
+++ atividade2.py
@@ -0,0 +1,10 @@
+contato = {}
+
+contato["nome"] = input("Digite o nome do contato: ")
+contato["telefone"] = input("Digite o telefone do contato: ")
+contato["email"] = input("Digite o email do contato: ")
+
+print("Informações do contato:")
+print("Nome:", contato["nome"])
+print("Telefone:", contato["telefone"])
+print("Email:", contato["email"])
